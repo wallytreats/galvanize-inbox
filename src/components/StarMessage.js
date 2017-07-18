c8b2c1d4610da82cbee67e7f80f8c97fae596c19
wallytreats <nicklawrenceemail@gmail.com>
2017-07-18 10:46:35 -0600

    added inbox files

---
+++ src/components/StarMessage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Message = ({message}) => {
+
+  console.log(message);
+
+  return (
+
+          <div className="col-xs-2">
+            <i className="star fa fa-star-o"></i>
+          </div>
+
+  )
+}
+
+export default Message
